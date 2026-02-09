8bf3aee14b3f0f45613e3e5bec91d002fc9b7c86
João G. R. Rocha <joaogabriel2r.profissional@hotmail.com>
2026-02-08 22:44:37 -0300

    feat: add GetBackdropImage component and update Main component layout

--- src/widgets/Main/Main.tsx
+++ src/widgets/Main/Main.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Main = () => {
   return (
-    <div>Main</div>
-  )
-}
+    <>
+      <div className="border-2 border-white">Capa</div>
+    </>
+  );
+};
 
-export default Main
+export default Main;
