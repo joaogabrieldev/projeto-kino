d9970a754cbc55c07eb3182f177ebd62f04ebe7b
joaogabrieldev0 <joaogabriel2r.profissional@hotmail.com>
2026-02-10 17:40:03 -0300

    chore: adicionar o use client no topo do arquivo de Provider

---
+++ src/providers/ReactQueryProvider.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { type ReactNode, useState } from "react";
+
+const ReactQueryProvider = ({ children }: { children: ReactNode }) => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+            retry: 1,
+            staleTime: 1000 * 60 * 5,
+          },
+        },
+      }),
+  );
+
+  console.log("🟢 React Query Provider Montado!");
+
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+};
+
+export default ReactQueryProvider;
