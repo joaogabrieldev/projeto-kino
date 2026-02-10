d120bba2d48c90b427982c5cc02fa4e7461d480f
joaogabrieldev0 <joaogabriel2r.profissional@hotmail.com>
2026-02-10 17:45:36 -0300

    fix: remover importação duplicada de globals.css e ajustar a estrutura do componente RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
+import "./globals.css";
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+
+import ReactQueryProvider from "@/providers/ReactQueryProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +27,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <ReactQueryProvider>{children}</ReactQueryProvider>
       </body>
     </html>
   );
