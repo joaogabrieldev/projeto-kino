f4028d9268883e5a92ec334812cdc2c2c709263f
joaogabrieldev0 <joaogabriel2r.profissional@hotmail.com>
2026-02-11 03:01:58 -0300

    feat: adicionar hooks para obter séries de TV e ajustar o tempo de expiração das consultas

--- src/hooks/useMovies.ts
+++ src/hooks/useMovies.ts
@@ -1,4 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
+import { data } from "motion/react-client";
 
 import type { MovieResponse } from "@/assets/types/movie";
 import { ENDPOINTS } from "@/constants/endpoints";
@@ -17,6 +18,83 @@ export const usePopularMovies = () => {
       return data;
     },
 
-    staleTime: 1000 * 60 * 5,
+    staleTime: 1000 * 60 * 6, // 6 minutos
+  });
+};
+
+export const useTopRatedMovies = () => {
+  return useQuery({
+    queryKey: ["movie", "top_rated"],
+    queryFn: async () => {
+      const { data } = await api.get<MovieResponse>(ENDPOINTS.MOVIES.TOP_RATED, {
+        params: {
+          page: 1,
+        },
+      });
+
+      return data;
+    },
+
+    staleTime: 1000 * 60 * 6,
+  });
+};
+
+export const useUpcomingMovies = () => {
+  return useQuery({
+    queryKey: ["movie", "upcoming"],
+    queryFn: async () => {
+      const { data } = await api.get<MovieResponse>(ENDPOINTS.MOVIES.UPCOMING, {
+        params: {
+          page: 1,
+        },
+      });
+
+      return data;
+    },
+
+    staleTime: 1000 * 60 * 6,
+  });
+};
+
+export const useNowPlayingMovies = () => {
+  return useQuery({
+    queryKey: ["movie", "now_playing"],
+    queryFn: async () => {
+      const { data } = await api.get<MovieResponse>(ENDPOINTS.MOVIES.NOW_PLAYING, {
+        params: {
+          page: 1,
+        },
+      });
+
+      return data;
+    },
+
+    staleTime: 1000 * 60 * 6,
+  });
+};
+
+export const useMovieRecommendations = (movieID: number) => {
+  return useQuery({
+    queryKey: ["movie-recommendations", movieID],
+    queryFn: async () => {
+      const { data } = await api.get<MovieResponse>(ENDPOINTS.MOVIES.RECOMMENDATIONS(movieID));
+      return data;
+    },
+
+    enabled: !!movieID && movieID > 0,
+    staleTime: 1000 * 60 * 60,
+  });
+};
+
+export const useMovieCredits = (movieID: number) => {
+  return useQuery({
+    queryKey: ["movie-credits", movieID],
+    queryFn: async () => {
+      const { data } = await api.get<MovieResponse>(ENDPOINTS.MOVIES.CREDITS(movieID));
+      return data;
+    },
+
+    enabled: !!movieID && movieID > 0,
+    staleTime: 1000 * 60 * 60,
   });
 };
--- src/hooks/useTVShows.ts
+++ src/hooks/useTVShows.ts
@@ -17,6 +17,81 @@ export const usePopularTVShow = () => {
       return data;
     },
 
-    staleTime: 1000 * 60 * 5, //? 5 minutos,
+    staleTime: 1000 * 60 * 6, //? 6 minutos,
+  });
+};
+
+export const useTopRatedTVShows = () => {
+  return useQuery({
+    queryKey: ["tv", "top_rated"],
+    queryFn: async () => {
+      const { data } = await api.get<TVShowResponse>(ENDPOINTS.TV.TOP_RATED, {
+        params: {
+          page: 1,
+        },
+      });
+
+      return data;
+    },
+
+    staleTime: 1000 * 60 * 6,
+  });
+};
+
+export const useOnTheAirTVShows = () => {
+  return useQuery({
+    queryKey: ["tv", "popular"],
+    queryFn: async () => {
+      const { data } = await api.get<TVShowResponse>(ENDPOINTS.TV.ON_THE_AIR, {
+        params: {
+          page: 1,
+        },
+      });
+
+      return data;
+    },
+
+    staleTime: 1000 * 60 * 6,
+  });
+};
+
+export const useAiringTodayTVShows = () => {
+  return useQuery({
+    queryKey: ["tv", "airing_today"],
+    queryFn: async () => {
+      const { data } = await api.get<TVShowResponse>(ENDPOINTS.TV.AIRING_TODAY, {
+        params: {
+          page: 1,
+        },
+      });
+
+      return data;
+    },
+  });
+};
+
+export const useTVShowsRecommendations = (tvShowID: number) => {
+  return useQuery({
+    queryKey: ["tv-recommendations", tvShowID],
+    queryFn: async () => {
+      const { data } = await api.get<TVShowResponse>(ENDPOINTS.TV.RECOMMENDATIONS(tvShowID));
+      return data;
+    },
+
+    enabled: !!tvShowID && tvShowID > 0,
+    staleTime: 1000 * 60 * 60,
+  });
+};
+
+export const useTVShowsCredits = (tvShowID: number) => {
+  return useQuery({
+    queryKey: ["tv-credits", tvShowID],
+    queryFn: async () => {
+      const { data } = await api.get<TVShowResponse>(ENDPOINTS.TV.CREDITS(tvShowID));
+      return data;
+    },
+
+    enabled: !!tvShowID && tvShowID > 0,
+    staleTime: 1000 * 60 * 60,
   });
 };
