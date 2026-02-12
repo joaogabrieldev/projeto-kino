97bebb47bfc0665d735c97f25aa042d9b97cb9f7
joaogabrieldev0 <joaogabriel2r.profissional@hotmail.com>
2026-02-12 02:40:11 -0300

    refactor: remover importações não utilizadas e estruturar o componente ContentRows

--- src/widgets/Content/Content.tsx
+++ src/widgets/Content/Content.tsx
@@ -1,6 +1,3 @@
-import { AnimatePresence, motion } from "motion/react";
-import React from "react";
-
 import Header from "../Header/Header";
 import Main from "./../Main/Main";
 
--- src/widgets/ContentCarousels/ContentCarousels.tsx
+++ src/widgets/ContentCarousels/ContentCarousels.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+"use client";
+
+import React, { useMemo } from "react";
+
+import { usePopularMovies, useTopRatedMovies } from "@/hooks/useMovies";
+import { usePopularTVShow, useTopRatedTVShows } from "@/hooks/useTVShows";
 
 const ContentRows = () => {
-  return <div>ContentRows</div>;
+  //? 1º Row
+  const { data: popularMoviesData } = usePopularMovies();
+  const { data: popularTVShowData } = usePopularTVShow();
+
+  //? 2º Row
+  const { data: topRatedMoviesData } = useTopRatedMovies();
+  const { data: topRatedTVShowData } = useTopRatedTVShows();
+
+  //? 3º Row
+
+  //? 4º Row
+
+  return <section>ContentRows</section>;
 };
 
 export default ContentRows;
