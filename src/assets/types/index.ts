14b1db7756796a50a6293cd5cb2a5884842186c7
joaogabrieldev0 <joaogabriel2r.profissional@hotmail.com>
2026-02-12 15:39:04 -0300

    feat: adicionar interfaces Genre e GenresResponse ao arquivo de tipos

--- src/assets/types/index.ts
+++ src/assets/types/index.ts
@@ -6,3 +6,18 @@ export type MovieItem = Movie & { media_type: "movie" };
 export type TVItem = TVShow & { media_type: "tv" };
 
 export type MediaItem = MovieItem | TVItem;
+
+export interface DiscoveryResponse {
+  page: number;
+  results: MediaItem[];
+  total_pages: number;
+  total_results: number;
+}
+
+export interface Genre {
+  id: number;
+  name: string;
+}
+export interface GenresResponse {
+  genres: Genre[];
+}
