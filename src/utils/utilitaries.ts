9e520d83e0a56c8c3daf7a829de174b6f09e7bd4
João G. R. Rocha <joaogabriel2r.profissional@hotmail.com>
2026-02-13 15:38:54 -0300

    feat: implementar função mixContentTypeArray para misturar filmes e séries

--- src/utils/utilitaries.ts
+++ src/utils/utilitaries.ts
@@ -4,7 +4,38 @@ export function shuffleArray<T>(array: T[]): T[] {
   return [...array].sort(() => Math.random() - 0.5);
 }
 
-export const mixContentTypeArray = (
+//* Misturar data de filmes e séries
+export function mixContentTypeArray(
   movies: MovieItem[] | null | undefined,
   tvShows: TVItem[] | null | undefined,
-): MediaItem[] => {};
+): MediaItem[] {
+  const safeMovies = movies || [];
+  const safeShows = tvShows || [];
+
+  const formattedMovies = safeMovies.map((item) => ({
+    ...item,
+    media_type: "movie" as const,
+  }));
+
+  const formattedTVShows = safeShows.map((item) => ({
+    ...item,
+    media_type: "tv" as const,
+  }));
+
+  const allContent = [...formattedMovies, ...formattedTVShows];
+
+  return shuffleArray(allContent);
+}
+
+export function mixMoviesData(
+  array1: MovieItem[],
+  array2: MovieItem[],
+  array3?: MovieItem[],
+): MediaItem[] {
+
+  const safeMovies1 = array1 || [];
+  const safeMovies2 = array2 || [];
+  const safeMovies3 = array3 || [];
+
+  
+}
