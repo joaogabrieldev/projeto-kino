1fa015824cee5ff8e594e85727a26aad3a2dc4d9
joaogabrieldev0 <joaogabriel2r.profissional@hotmail.com>
2026-02-13 03:02:04 -0300

    feat: adicionar função mixContentTypeArray para manipulação de filmes e séries

--- src/utils/utilitaries.ts
+++ src/utils/utilitaries.ts
@@ -1,3 +1,10 @@
+import { MediaItem, MovieItem, TVItem } from "@/assets/types";
+
 export function shuffleArray<T>(array: T[]): T[] {
   return [...array].sort(() => Math.random() - 0.5);
 }
+
+export const mixContentTypeArray = (
+  movies: MovieItem[] | null | undefined,
+  tvShows: TVItem[] | null | undefined,
+): MediaItem[] => {};
