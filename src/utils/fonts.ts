d31cff49527228418591aa2bf5a3a3fca43d38a7
João G. R. Rocha <joaogabriel2r.profissional@hotmail.com>
2026-02-15 16:31:44 -0300

    feat: adicionar fonte Alte Haas Grotesk ao projeto

--- src/utils/fonts.ts
+++ src/utils/fonts.ts
@@ -1,6 +1,24 @@
 import { Ubuntu } from "next/font/google";
+import localFont from "next/font/local";
 
 export const ubuntu = Ubuntu({
   weight: ["300", "400", "500", "700"],
   subsets: ["latin"],
+});
+
+export const alteHaasGrotesk = localFont({
+  src: [
+    {
+      path: "../../assets/fonts/AlteHaasGrotesk/AlteHaasGroteskRegular.ttf",
+      weight: "100 500",
+      style: "normal",
+    },
+    {
+      path: "../../assets/fonts/AlteHaasGrotesk/AlteHaasGroteskBold.ttf",
+      weight: "500 900",
+      style: "normal",
+    },
+  ],
+  variable: "--font-alte-Haas-Grotesk",
+  display: "swap",
 });
