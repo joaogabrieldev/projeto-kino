1eb4e629ca2ca4a9df254012442e2fc7fdbbc2e8
joaogabrieldev0 <joaogabriel2r.profissional@hotmail.com>
2026-02-15 21:11:23 -0300

    feat: reorganizar e atualizar a exibição de conteúdos no componente ContentRows

--- src/widgets/ContentCarousels/ContentCarousels.tsx
+++ src/widgets/ContentCarousels/ContentCarousels.tsx
@@ -23,38 +23,56 @@ const ContentRows = () => {
   //? 1º Row
   const { data: popularMoviesData } = usePopularMovies();
   const { data: popularTVShowData } = usePopularTVShow();
+  const popularContent = mixContentTypeArray(popularMoviesData, popularTVShowData, 60);
 
   //? 2º Row
   const { data: topRatedMoviesData } = useTopRatedMovies();
-  const { data: topRatedTVShowData } = useTopRatedTVShows();
 
   //? 3º Row
-  const { data: upcomingMoviesData } = useUpcomingMovies();
+  const { data: topRatedTVShowData } = useTopRatedTVShows();
 
   //? 4º Row
-  const { data: nowPlayingMoviesData } = useNowPlayingMovies();
-  const { data: onTheAirTVShowData } = useOnTheAirTVShows();
+  const { data: upcomingMoviesData } = useUpcomingMovies();
 
   //? 5º Row
+  const { data: nowPlayingMoviesData } = useNowPlayingMovies();
+  const { data: onTheAirTVShowData } = useOnTheAirTVShows();
+  const nowPlayingContent = useMemo(() => {
+    const nowPlayingContent = nowPlayingMoviesData?.filter((movie) => {
+      return (movie.popularity ?? 0) > 50;
+    });
+
+    const onTheAirContent = onTheAirTVShowData?.filter((show) => {
+      return (show.popularity ?? 0) > 50;
+    });
+
+    return mixContentTypeArray(nowPlayingContent, onTheAirContent, 60);
+  }, [nowPlayingMoviesData, onTheAirTVShowData]);
+
+  //? 6º Row
   const { data: airingTodayTVShowsData } = useAiringTodayTVShows();
 
-  //? 6º Row - Ação
+  //? 7º Row - Ação
   const { data: actionGenreMoviesData } = useMediaByGenre("movie", MOVIE_GENRES.ACTION);
   const { data: actionGenreTVShowData } = useMediaByGenre("tv", TV_GENRES.ACTION_AND_ADVENTURE);
 
-  //? 7º Row - Sci-Fi
+  //? 8º Row - Sci-Fi
   const { data: sciFiGenreMoviesData } = useMediaByGenre("movie", MOVIE_GENRES.SCIENCE_FICTION);
   const { data: sciFiGenreTVShowsData } = useMediaByGenre("tv", TV_GENRES.SCI_FI_AND_FANTASY);
 
-  const sciFiContent = mixContentTypeArray(
-    sciFiGenreMoviesData?.slice().sort((a, b) => (b.popularity ?? 0) - (a.popularity ?? 0)),
-    sciFiGenreTVShowsData?.slice().sort((a, b) => (b.popularity ?? 0) - (a.popularity ?? 0)),
-    60,
-  );
+  const sciFiContent = mixContentTypeArray(sciFiGenreMoviesData, sciFiGenreTVShowsData, 100);
 
   return (
-    <section className="-mt-28">
-      <ContentRow title="Teste" data={sciFiContent} />
+    <section className="-mt-30 flex flex-col gap-2">
+      <ContentRow title="🔥 O Que Todo Mundo Está Assistindo" data={popularContent} />
+
+      <ContentRow title="🍿 Obras-Primas do Cinema" data={topRatedMoviesData || []} />
+
+      <ContentRow title="🏆 As Mais Bem Avaliadas" data={topRatedTVShowData || []} />
+
+      <ContentRow title="👀 Fique de Olho: Vem Aí" data={upcomingMoviesData || []} />
+
+      <ContentRow title="📺 Novidades na Tela" data={nowPlayingContent} />
     </section>
   );
 };
