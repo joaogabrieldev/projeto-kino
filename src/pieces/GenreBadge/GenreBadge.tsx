4ea19c48fd727cb2ae66d70185aeb9ee5e6e4926
joaogabrieldev0 <joaogabriel2r.profissional@hotmail.com>
2026-02-20 03:45:03 -0300

    feat(GenreBadge): implementa componente GenreBadge com lógica de seleção e estilo dinâmico

--- src/pieces/GenreBadge/GenreBadge.tsx
+++ src/pieces/GenreBadge/GenreBadge.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
-const GenreBadge = () => {
-  return <div></div>;
+import { onest } from "@/utils/fonts";
+
+interface IGenreBadge {
+  genreTitle: string;
+  isSelected: boolean;
+  onClick: () => void;
+}
+
+const GenreBadge = ({ genreTitle, isSelected, onClick }: IGenreBadge) => {
+  const selectedStyle = isSelected
+    ? "bg-[#8b0000] text-[#d4af37] hover:text-[#d4af37] font-semibold border-[#8b0000]"
+    : "border border-white text-white";
+
+  return (
+    <div
+      className={`flex w-fit cursor-pointer items-center justify-center rounded-xl border px-4 py-0.5 ${selectedStyle}`}
+      onClick={onClick}
+    >
+      <span className={`${onest.className}`}>{genreTitle}</span>
+    </div>
+  );
 };
 
 export default GenreBadge;
