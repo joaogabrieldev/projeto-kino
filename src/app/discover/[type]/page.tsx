f960ee75cacb70f0634e82fcbbcb4b5c0275b157
joaogabrieldev0 <joaogabriel2r.profissional@hotmail.com>
2026-02-20 17:40:11 -0300

    refactor(GenericDiscoverPage): simplifica a estrutura do componente removendo lógica desnecessária e utilizando DiscoverHomePage

--- src/app/discover/[type]/page.tsx
+++ src/app/discover/[type]/page.tsx
@@ -1,61 +1,17 @@
-"use client";
+import React, { use } from "react";
 
-import Image from "next/image";
-import React, { use, useEffect, useMemo, useState } from "react";
+import DiscoverHomePage from "@/templates/DiscoverHomePage/DiscoverHomePage";
 
-import { MediaItem } from "@/assets/types";
-import FilterCard from "@/components/FilterCard/FilterCard";
-import { ENDPOINTS } from "@/constants/endpoints";
-import { GENRES_PT_BR, getGenreId } from "@/constants/genres";
-import { useDiscoverMedia } from "@/hooks/useContent";
-import { onest } from "@/utils/fonts";
-import GridContents from "@/widgets/GridContents/GridContents";
-
-interface IDiscoverHomePage {
+interface IGenericDiscoverPage {
   params: Promise<{
     type: "movie" | "tv";
   }>;
 }
 
-const DiscoverHomePage = ({ params }: IDiscoverHomePage) => {
-  const [currentPage, setCurrentPage] = useState<number>(1);
-
-  const [selectedGenres, setSelectedGenres] = useState<string[]>([]);
-
+const GenericDiscoverPage = ({ params }: IGenericDiscoverPage) => {
   const { type } = use(params);
 
-  const genresForApi = useMemo(() => {
-    if (selectedGenres.length === 0) return undefined;
-
-    return selectedGenres
-      .map((genreName) => getGenreId(genreName))
-      .filter((id): id is number => id !== undefined);
-  }, [selectedGenres]);
-
-  const { data: allMediaData } = useDiscoverMedia(type, {
-    page: currentPage,
-    genres: genresForApi,
-  });
-
-  const pageTitle = type === "movie" ? "Todos os Filmes" : "Todas as Séries";
-
-  const genreList = type === "movie" ? GENRES_PT_BR.MOVIES : GENRES_PT_BR.TV_SHOWS;
-
-  return (
-    <div className="mt-28 mb-14 flex justify-center gap-x-4">
-      {/* <h1 className="text-white">Você está em {type}</h1> */}
-      <FilterCard
-        genreList={genreList}
-        currentPage={currentPage}
-        onPageChange={setCurrentPage}
-        totalPages={allMediaData?.totalPages ?? 1}
-        pageTitle={pageTitle}
-        selectedGenres={selectedGenres}
-        onGenreSelect={setSelectedGenres}
-      />
-      <GridContents results={allMediaData?.results ?? []} />
-    </div>
-  );
+  return <DiscoverHomePage type={type} />;
 };
 
-export default DiscoverHomePage;
+export default GenericDiscoverPage;
