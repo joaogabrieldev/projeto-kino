80e4220b2d8fcbf94ebc1b4de9381521ecdc1426
João G. R. Rocha <joaogabriel2r.profissional@hotmail.com>
2026-02-19 23:48:22 -0300

    feat(GenreBadges): cria componente GenreBadges com interface para tipos de mídia

---
+++ src/widgets/GenreBadges/GenreBadges.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface IGenreBadges {
+  type: "movie" | "tv";
+}
+
+const GenreBadges = () => {
+  return <div>GenreBadges</div>;
+};
+
+export default GenreBadges;
