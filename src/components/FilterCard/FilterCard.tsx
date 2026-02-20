30540264ade7d59c4cf04fb4b4a9789e83ab06ac
João G. R. Rocha <joaogabriel2r.profissional@hotmail.com>
2026-02-19 23:47:57 -0300

    feat(FilterCard): adiciona título dinâmico ao componente FilterCard

--- src/components/FilterCard/FilterCard.tsx
+++ src/components/FilterCard/FilterCard.tsx
@@ -7,12 +7,16 @@ interface IFilterCardProps {
   totalPages: number;
   currentPage: number;
   onPageChange: React.Dispatch<React.SetStateAction<number>>;
+  pageTitle: string;
 }
 
-const FilterCard = ({ totalPages, currentPage, onPageChange }: IFilterCardProps) => {
+const FilterCard = ({ totalPages, currentPage, onPageChange, pageTitle }: IFilterCardProps) => {
   return (
-    <div className="h-fit border-2 border-white text-white select-none">
-      <div>Teste</div>
+    <div className="-mt-4 h-fit border-2 border-white text-white select-none">
+      <div className="pb-4">
+        <span className="pl-6 text-2xl font-bold">{pageTitle}</span>
+      </div>
+      <div></div>
       <div className="border-2 border-white">
         <Paginations.PaginationCardDefault
           page={currentPage}
