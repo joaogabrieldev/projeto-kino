c37ecc277f6b38458dd987733c98992bee876102
joaogabrieldev0 <joaogabriel2r.profissional@hotmail.com>
2026-02-22 14:27:53 -0300

    feat(PersonPage): cria componente PersonPage com integração do PersonHomePage

---
+++ src/app/person/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import PersonHomePage from "@/widgets/PersonHomePage/PersonHomePage";
+
+const PersonPage = () => {
+  return (
+    <div className="mt-30 mb-15">
+      <PersonHomePage />
+    </div>
+  );
+};
+
+export default PersonPage;
