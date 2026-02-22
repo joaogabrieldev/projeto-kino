d47926574b1aa094cfd202d3777bb29fc82fcc61
joaogabrieldev0 <joaogabriel2r.profissional@hotmail.com>
2026-02-22 14:34:20 -0300

    feat(ContentCard): adiciona suporte para propriedades de largura e altura, e refatora a lógica de renderização do componente

--- src/pieces/ContentCard/ContentCard.tsx
+++ src/pieces/ContentCard/ContentCard.tsx
@@ -9,28 +9,46 @@ interface IContentCardProps {
   linkHref: string;
   imageSrc: string;
   alt: string;
+  isCarousel?: boolean;
+  width?: number;
+  height?: number;
 }
 
-const ContentCard = ({ itemID, linkHref, imageSrc, alt }: IContentCardProps) => {
+const ContentCard = ({
+  itemID,
+  linkHref,
+  imageSrc,
+  alt,
+  isCarousel = true,
+  width,
+  height = 750,
+}: IContentCardProps) => {
+  const cardContent = (
+    <Link href={linkHref} className="block h-full w-full">
+      <div className="relative z-20 aspect-2/3 w-full cursor-pointer overflow-hidden rounded-sm bg-zinc-900 shadow-md shadow-zinc-900 transition-transform duration-300 hover:z-50 hover:scale-105">
+        <Image
+          src={imageSrc}
+          alt={alt}
+          width={width}
+          height={height}
+          className="rounded-lg object-cover"
+          sizes="(max-width: 768px) 50vw, (max-width: 1200px) 33vw, 20vw"
+        />
+      </div>
+    </Link>
+  );
+
+  if (!isCarousel) {
+    return cardContent;
+  }
+
   return (
-    <>
-      <CarouselItem
-        key={itemID}
-        className="basis-1/2 pl-2 md:basis-1/3 md:pl-4 lg:basis-1/4 xl:basis-1/5"
-      >
-        <Link href={linkHref}>
-          <div className="relative aspect-2/3 cursor-pointer overflow-hidden rounded-sm bg-zinc-900 shadow-md transition-transform duration-300 hover:z-50 hover:scale-105">
-            <Image
-              src={imageSrc}
-              alt={alt}
-              fill
-              className="object-cover"
-              sizes="(max-width: 768px) 50vw, (max-width: 1200px) 33vw, 20vw"
-            />
-          </div>
-        </Link>
-      </CarouselItem>
-    </>
+    <CarouselItem
+      key={itemID}
+      className="basis-1/2 pl-2 md:basis-1/3 md:pl-4 lg:basis-1/4 xl:basis-1/5"
+    >
+      {cardContent}
+    </CarouselItem>
   );
 };
 
