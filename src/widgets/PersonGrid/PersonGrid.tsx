14af03f6d0cdba68c3894922f69815d89681da4f
joaogabrieldev0 <joaogabriel2r.profissional@hotmail.com>
2026-02-22 14:38:02 -0300

    feat(PersonGrid): adiciona componente PersonGrid para exibir uma grade de PersonCards

---
+++ src/widgets/PersonGrid/PersonGrid.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import PersonCard, { IPerson } from "@/components/PersonCard/PersonCard";
+
+interface IPersonGridProps {
+  results: IPerson[];
+}
+
+const PersonGrid = ({ results }: IPersonGridProps) => {
+  if (!results || results.length === 0) return null;
+
+  return (
+    <div className="grid gap-x-10 gap-y-8 select-none md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {results.map((person) => {
+        return <PersonCard key={person.id} person={person} />;
+      })}
+    </div>
+  );
+};
+
+export default PersonGrid;
