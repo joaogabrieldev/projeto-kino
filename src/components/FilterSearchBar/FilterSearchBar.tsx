54b3b8784c347a3e4a9e25ef1c47720847f2ac3d
joaogabrieldev0 <joaogabriel2r.profissional@hotmail.com>
2026-02-22 14:32:31 -0300

    feat(FilterSearchBar): adiciona suporte para busca de pessoas e refatora lógica de pesquisa

--- src/components/FilterSearchBar/FilterSearchBar.tsx
+++ src/components/FilterSearchBar/FilterSearchBar.tsx
@@ -1,36 +1,59 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import React, { ChangeEvent, useEffect, useState } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import React, { ChangeEvent, useEffect, useRef, useState } from "react";
 import { FiSearch } from "react-icons/fi";
 
 import { useDebounce } from "@/hooks/useDebounce";
 import { onest } from "@/utils/fonts";
 
-const FilterSearchBar = () => {
-  const router = useRouter();
+interface IFilterSearchBar {
+  isPersonSearch?: boolean;
+}
 
-  const [inputValue, setInputValue] = useState("");
+const FilterSearchBar = ({ isPersonSearch }: IFilterSearchBar) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const initialQuery = searchParams.get("q") || "";
+  const [inputValue, setInputValue] = useState(initialQuery);
 
   const debouncedQuery = useDebounce(inputValue, 500);
 
+  const isMounted = useRef(false);
+
   useEffect(() => {
+    if (!isMounted.current) {
+      isMounted.current = true;
+      return;
+    }
+
     const sanitizedQuery = debouncedQuery.trim();
 
     if (sanitizedQuery) {
       const params = new URLSearchParams();
       params.set("q", sanitizedQuery);
 
-      router.replace(`/search?${params.toString()}`);
+      if (isPersonSearch) {
+        router.replace(`/search/person?${params.toString()}`);
+      } else {
+        router.replace(`/search?${params.toString()}`);
+      }
     } else {
+      if (pathname.includes("/search")) {
+        router.replace(isPersonSearch ? `/search/person` : `/search`);
+      }
     }
-  }, [debouncedQuery, router]);
+  }, [debouncedQuery, isPersonSearch, pathname, router]);
+
+  const placeholder = isPersonSearch ? "Busque por Artistas..." : "Busque por Títulos...";
 
   return (
     <div className="flex w-full max-w-sm items-center rounded-full border border-gray-700 bg-gray-800 px-4 py-2 transition-colors focus-within:border-gray-200">
       <input
         type="text"
-        placeholder="Busque por Títulos"
+        placeholder={placeholder}
         value={inputValue}
         onChange={(event) => setInputValue(event.target.value)}
         className={`w-full bg-transparent text-white placeholder-gray-400 outline-none ${onest.className}`}
