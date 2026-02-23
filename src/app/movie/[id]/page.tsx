91396a82bf5f88ac4d12c58d8d89ee9855ac7f27
joaogabrieldev0 <joaogabriel2r.profissional@hotmail.com>
2026-02-23 14:36:43 -0300

    feat(MovieIDPage): integra componente ContentDetailsPage para exibir detalhes do filme

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,4 +1,10 @@
+"use client";
+
 import React, { use } from "react";
+
+import { useMovieByID } from "@/hooks/useMovies";
+import Loading from "@/pieces/Loading/Loading";
+import ContentDetailsPage from "@/widgets/ContentDetailsPage/ContentDetailsPage";
 
 interface IMovieIDPage {
   params: Promise<{
@@ -9,7 +15,11 @@ interface IMovieIDPage {
 const MovieIDPage = ({ params }: IMovieIDPage) => {
   const { id } = use(params);
 
-  return <div className="mt-20 text-white">ID do Filme: {id}</div>;
+  return (
+    <div className="mt-20 text-white">
+      <ContentDetailsPage key={id} id={id} type="movie" />
+    </div>
+  );
 };
 
 export default MovieIDPage;
