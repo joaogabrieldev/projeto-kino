f08733bad7e93a90d20ed32c3a1072e3ef7394b3
joaogabrieldev0 <joaogabriel2r.profissional@hotmail.com>
2026-02-23 14:40:36 -0300

    feat(LoadingPoster): adiciona componente LoadingPoster para exibição de carregamento

---
+++ src/pieces/LoadingPoster/LoadingPoster.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const LoadingPoster = () => {
+  return (
+    <div className="absolute z-10 h-[513px] w-[342px] animate-pulse rounded-2xl bg-zinc-800"></div>
+  );
+};
+
+export default LoadingPoster;
