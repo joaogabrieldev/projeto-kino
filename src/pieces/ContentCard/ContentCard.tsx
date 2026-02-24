f467411e553b9dee122c02f3be194c04ba604b3a
João G. R. Rocha <joaogabriel2r.profissional@hotmail.com>
2026-02-24 19:34:14 -0300

    refactor(ContentCard): remove lógica de carrossel e simplifica o componente

--- src/pieces/ContentCard/ContentCard.tsx
+++ src/pieces/ContentCard/ContentCard.tsx
@@ -2,14 +2,11 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-import { CarouselItem } from "@/components/ui/carousel";
-
 interface IContentCardProps {
   itemID: number | string;
   linkHref: string;
   imageSrc: string;
   alt: string;
-  isCarousel?: boolean;
   width?: number;
   height?: number;
 }
@@ -19,13 +16,13 @@ const ContentCard = ({
   linkHref,
   imageSrc,
   alt,
-  isCarousel = true,
   width,
   height = 750,
 }: IContentCardProps) => {
-  const cardContent = (
+  return (
     <Link href={linkHref} className="block h-full w-full">
-      <div className="relative z-20 aspect-2/3 w-full cursor-pointer overflow-hidden rounded-sm bg-zinc-900 shadow-md shadow-zinc-900 transition-transform duration-300 hover:z-50 hover:scale-105">
+      {/* Dica: no Tailwind puro geralmente se usa aspect-[2/3] com colchetes */}
+      <div className="relative z-20 aspect-[2/3] w-full cursor-pointer overflow-hidden rounded-sm bg-zinc-900 shadow-md shadow-zinc-900 transition-transform duration-300 hover:z-50 hover:scale-105">
         <Image
           src={imageSrc}
           alt={alt}
@@ -37,19 +34,6 @@ const ContentCard = ({
         />
       </div>
     </Link>
-  );
-
-  if (!isCarousel) {
-    return cardContent;
-  }
-
-  return (
-    <CarouselItem
-      key={itemID}
-      className="basis-1/2 pl-2 md:basis-1/3 md:pl-4 lg:basis-1/4 xl:basis-1/5"
-    >
-      {cardContent}
-    </CarouselItem>
   );
 };
 
