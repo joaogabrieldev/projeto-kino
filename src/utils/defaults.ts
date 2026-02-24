204152c22bd2216bac748d9dd9d8fa978dfb2f5d
joaogabrieldev0 <joaogabriel2r.profissional@hotmail.com>
2026-02-24 17:12:22 -0300

    feat(defaults): adiciona estilo de fonte "semibold" à seleção padrão

--- src/utils/defaults.ts
+++ src/utils/defaults.ts
@@ -1,1 +1,1 @@
-export const defaultSelection = `selection:bg-[#8b0000] selection:font-bold selection:text-yellow-500`;
+export const defaultSelection = `selection:bg-[#8b0000] selection:font-bold selection:text-yellow-500 selection:font-semibold`;
