6e53566b15edf5051dacf1e3446d60585857c01a
joaogabrieldev0 <joaogabriel2r.profissional@hotmail.com>
2026-02-24 17:11:06 -0300

    feat(PersonCard): melhora a exibição quando não há imagem do ator, adicionando ícone e texto

--- src/components/PersonCard/PersonCard.tsx
+++ src/components/PersonCard/PersonCard.tsx
@@ -79,8 +79,25 @@ const PersonCard = ({ person }: PersonCardProps) => {
               className="object-cover transition-transform duration-400 ease-in-out group-hover:scale-105"
             />
           ) : (
-            <div className="flex h-full w-full items-center justify-center text-gray-500">
-              Sem foto
+            <div className="flex h-full w-full flex-col items-center justify-center text-gray-500">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="lucide lucide-user-icon lucide-user"
+              >
+                <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
+                <circle cx="12" cy="7" r="4" />
+              </svg>
+              <div>
+                <span className="font-semibold">Sem foto</span>
+              </div>
             </div>
           )}
         </div>
