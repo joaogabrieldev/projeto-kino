c1fccd66187c5b79cc5f83010e42267bcc2cd2fa
joaogabrieldev0 <joaogabriel2r.profissional@hotmail.com>
2026-02-25 02:58:55 -0300

    fix(ContentCard): ajusta link para incluir largura mínima e remove propriedades de largura e altura

--- src/pieces/ContentCard/ContentCard.tsx
+++ src/pieces/ContentCard/ContentCard.tsx
@@ -20,14 +20,12 @@ const ContentCard = ({
   height = 750,
 }: IContentCardProps) => {
   return (
-    <Link href={linkHref} className="block h-full w-full">
-      {/* Dica: no Tailwind puro geralmente se usa aspect-[2/3] com colchetes */}
+    <Link href={linkHref} className="block h-full w-full min-w-[300px]">
       <div className="relative z-20 aspect-[2/3] w-full cursor-pointer overflow-hidden rounded-sm bg-zinc-900 shadow-md shadow-zinc-900 transition-transform duration-300 hover:z-50 hover:scale-105">
         <Image
           src={imageSrc}
           alt={alt}
-          width={width}
-          height={height}
+          fill
           className="rounded-lg object-cover"
           sizes="(max-width: 768px) 50vw, (max-width: 1200px) 33vw, 20vw"
           loading="eager"
