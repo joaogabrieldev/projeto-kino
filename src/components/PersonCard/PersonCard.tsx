917767bce56c3ae1e2b4f525eff8c7dc8f24ba9d
João G. R. Rocha <joaogabriel2r.profissional@hotmail.com>
2026-02-26 00:41:01 -0300

    fix(PersonCard): corrige atributos SVG para usar camelCase

--- src/components/PersonCard/PersonCard.tsx
+++ src/components/PersonCard/PersonCard.tsx
@@ -86,9 +86,9 @@ const PersonCard = ({ person }: PersonCardProps) => {
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 className="lucide lucide-user-icon lucide-user"
               >
                 <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
