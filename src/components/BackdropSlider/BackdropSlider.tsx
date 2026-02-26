57f6f6e58354918196d72f0bc6b1618146180c89
joaogabrieldev0 <joaogabriel2r.profissional@hotmail.com>
2026-02-26 14:59:44 -0300

    feat(BackdropSlider): adiciona suporte a temas claro e escuro com estilos dinâmicos

--- src/components/BackdropSlider/BackdropSlider.tsx
+++ src/components/BackdropSlider/BackdropSlider.tsx
@@ -7,6 +7,7 @@ import { AnimatePresence } from "motion/react";
 import { motion } from "motion/react";
 import Image from "next/image";
 import Link from "next/link";
+import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 
 import { MediaItem } from "@/assets/types";
@@ -20,6 +21,8 @@ interface IBackdropSlider {
 
 export default function BackdropSlider({ content }: IBackdropSlider) {
   const [index, setIndex] = useState(0);
+
+  const { theme } = useTheme();
 
   useEffect(() => {
     if (content.length === 0) return;
@@ -41,8 +44,23 @@ export default function BackdropSlider({ content }: IBackdropSlider) {
 
   const mediaType = currentContent.media_type;
 
+  const isDark = theme === "dark";
+
+  const themeSet = {
+    background: isDark ? "bg-black" : "bg-[#CFCFCF]",
+    fade1: isDark ? "from-[#141414] via-[#141414]/50" : "from-[#BABABA] via-[#BABABA]/30",
+    fade2: isDark ? "from-[#141414] via-transparent " : "from-[#C4C4C4]/50 via-[#C4C4C4]/10",
+    title: isDark ? "text-[#d4af37]" : "text-[#a10000]",
+    overview: isDark ? "text-gray-300" : "text-gray-800 font-semibold",
+    detailsButton: isDark
+      ? "border-white bg-white text-black shadow-lg shadow-black"
+      : "border-zinc-800 bg-zinc-800 px-6 py-2 text-white shadow-lg shadow-gray-400",
+  };
+
   return (
-    <section className="relative z-0 h-screen w-full overflow-hidden bg-black select-none">
+    <section
+      className={`relative z-0 h-screen w-full overflow-hidden ${themeSet.background} select-none`}
+    >
       <AnimatePresence mode="popLayout">
         <motion.div
           key={currentContent.id}
@@ -62,9 +80,13 @@ export default function BackdropSlider({ content }: IBackdropSlider) {
             sizes="100vw"
           />
 
-          <div className="absolute bottom-0 z-10 h-[80vh] w-full bg-linear-to-t from-[#141414] via-[#141414]/50 to-transparent" />
+          <div
+            className={`absolute bottom-0 z-10 h-[80vh] w-full bg-linear-to-t ${themeSet.fade1} to-transparent`}
+          />
 
-          <div className="absolute inset-0 bg-linear-to-r from-[#000000] via-transparent to-transparent blur-2xl" />
+          <div
+            className={`absolute inset-0 bg-linear-to-r ${themeSet.fade2} to-transparent blur-2xl`}
+          />
         </motion.div>
       </AnimatePresence>
 
@@ -77,12 +99,14 @@ export default function BackdropSlider({ content }: IBackdropSlider) {
             exit={{ opacity: 0, y: -20 }}
             transition={{ duration: 0.5, delay: 0.2 }}
           >
-            <h1 className="movie-title text-5xl font-black tracking-tighter text-white uppercase drop-shadow-2xl select-none md:text-7xl">
+            <h1
+              className={`movie-title text-5xl font-black tracking-tighter uppercase drop-shadow-2xl select-none md:text-7xl ${themeSet.title} `}
+            >
               {title}
             </h1>
 
             <p
-              className={`mt-4 line-clamp-3 max-w-2xl text-lg text-gray-300 md:text-xl ${defaultSelection}`}
+              className={`mt-4 line-clamp-3 max-w-2xl text-lg md:text-xl ${defaultSelection} ${themeSet.overview} `}
             >
               {currentContent.overview}
             </p>
@@ -92,7 +116,7 @@ export default function BackdropSlider({ content }: IBackdropSlider) {
         <div>
           <Link
             href={`/${mediaType}/${currentContent.id}`}
-            className="flex w-fit flex-row gap-1 rounded-lg border-2 border-white bg-white px-6 py-2 text-black shadow-lg shadow-black"
+            className={`flex w-fit flex-row gap-1 rounded-lg border-2 px-6 py-2 ${themeSet.detailsButton} `}
           >
             <span>
               <Info width={32} className="antialiased" />
