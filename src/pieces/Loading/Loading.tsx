b8bc7e075c157e598d66d42033fef2fab4fb45e8
João G. R. Rocha <joaogabriel2r.profissional@hotmail.com>
2026-02-27 19:45:42 -0300

    fix(Loading): adiciona cores de texto para melhor legibilidade, consistência e suporte a temas

--- src/pieces/Loading/Loading.tsx
+++ src/pieces/Loading/Loading.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const Loading = () => {
   return (
     <div className="absolute top-1/2 left-1/2 -translate-1/2">
-      <div className="loader"></div>
+      <div className="loader text-[#a10000] dark:text-[#D4AF37]"></div>
     </div>
   );
 };
