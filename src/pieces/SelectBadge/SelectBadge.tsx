c900f88525c5e9f030ad7116e86ebf2cd6f6e97b
joaogabrieldev0 <joaogabriel2r.profissional@hotmail.com>
2026-02-27 17:20:57 -0300

    fix(SelectBadge): corrige estilos de seleção para melhor legibilidade
    feat(SelectBadge): adiciona suporte a temas

--- src/pieces/SelectBadge/SelectBadge.tsx
+++ src/pieces/SelectBadge/SelectBadge.tsx
@@ -10,8 +10,8 @@ interface ISelectBadge {
 
 const SelectBadge = ({ genreTitle, isSelected, onClick }: ISelectBadge) => {
   const selectedStyle = isSelected
-    ? "bg-[#8b0000] text-[#d4af37] hover:text-[#d4af37] font-semibold border-[#8b0000]"
-    : "border border-white text-white";
+    ? "bg-[#8b0000] text-gray-300 hover:text-gray-300 font-semibold border-[#8b0000] dark:text-[#d4af37] dark:hover:text-[#d4af37]"
+    : "border border-[#a10000] text-[#a10000] dark:border-white dark:text-white";
 
   return (
     <div
