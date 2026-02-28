1d54049b894f599ce6d52a426925caa265125d73
joaogabrieldev0 <joaogabriel2r.profissional@hotmail.com>
2026-02-28 00:39:33 -0300

    fix(CastCard): reorganiza imports

--- src/components/CastCard/CastCard.tsx
+++ src/components/CastCard/CastCard.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 import { onest } from "@/utils/fonts";
-import Link from "next/link";
 
 interface ICastCard {
   profileURL: string;
