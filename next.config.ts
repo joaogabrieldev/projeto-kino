80db9d4d05780e3272585be27d62a19222a64061
joaogabrieldev0 <joaogabriel2r.profissional@hotmail.com>
2026-03-02 10:19:04 -0300

    style(next.config): adicionar tipo de configuração para o Next.js

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
+/** @type {import('next').NextConfig} */
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  unoptimized: true,
   images: {
     remotePatterns: [
       {
